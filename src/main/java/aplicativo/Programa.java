7b9ce135ac5426c1c7e62bf07f1edb6a11d7f62f
Bruno Farias <bruno43ist@yahoo.com.br>
2020-03-25 14:42:51 -0300

    Primeiro Commit

---
+++ src/main/java/aplicativo/Programa.java
@@ -0,0 +1,31 @@
+package aplicativo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torrer", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println("pronto");
+		
+		em.close();
+		emf.close();
+
+
+	}
+
+}
